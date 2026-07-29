2c62cb290729aa53deac7ecab2f28ce8b698d7c4
Norzuiso <josueaguilarjnai@gmail.com>
2026-07-29 13:29:00 -0600

    feat: create logic of end state and add waiting connections http function

---
+++ internal/simulation/stateEnd.go
@@ -0,0 +1,49 @@
+package simulation
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Norzuiso/orchestrator/internal/models"
+	"github.com/Norzuiso/orchestrator/internal/utils"
+	pb "github.com/Norzuiso/protocol/gen/go/proto/orchestrator/v1"
+)
+
+type StateEnd struct {
+	SimulationEngine *SimulationEngine
+}
+
+func (s *StateEnd) StartState() {
+	log.Printf("State: %v", s.GetStateName())
+	_ = s.SendMsg(nil, nil)
+}
+
+func (s *StateEnd) GetStateName() string {
+	return utils.FinishingStr
+}
+
+func (s *StateEnd) ReadMsg(msg *pb.Message, conn *models.Connection) error {
+	return nil
+}
+
+func (s *StateEnd) SendMsg(msg *pb.Message, conn *models.Connection) error {
+	clientStreams := s.SimulationEngine.ClientService.ClientStreams
+	for _, client := range clientStreams {
+		client.ErrCh <- fmt.Errorf("End of simulation")
+	}
+	s.SimulationEngine.Orchestrator.ClientsRequest = make([]int64, 0)
+	s.SimulationEngine.Orchestrator.ClientsResponse = map[int64]*pb.Message{}
+	return nil
+}
+
+func (s *StateEnd) GetNextState() (utils.State, error) {
+	return nil, fmt.Errorf("No more states. This is the end of the simulation")
+}
+
+func (s *StateEnd) IsMsgTypeAllowIt(msg *pb.Message) bool {
+	return false
+}
+
+func NewStateEnd(s *SimulationEngine) *StateEnd {
+	return &StateEnd{SimulationEngine: s}
+}
