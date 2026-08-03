b17207a6aa6e3bb1f721c2996313fabec7d015b7
Norberto Aguilar <josueaguilarjnai@gmail.com>
2026-08-03 15:06:11 -0600

    fix: remove unuse phase

--- internal/orchestrator/orquestrator.go
+++ internal/orchestrator/orquestrator.go
@@ -6,23 +6,16 @@ import (
 	"slices"
 	"strconv"
 	"sync"
-
-	pb "github.com/Norzuiso/protocol/gen/go/proto/orchestrator/v1"
 )
 
 type Orchestrator struct {
 	Seed        int64
 	Epoch       int64
 	MaxOfEpochs int64
+	SeedEpoch   int64
 
-	ActiveClients            map[int64]*pb.Client
-	ClientToClientConnection map[int64]*pb.ClientConnectionList
-	ClientsResponse          map[int64]*pb.Message
-
-	mu sync.Mutex
-
+	mu             sync.Mutex
 	ClientsRequest []int64
-	SeedEpoch      int64
 }
 
 func (o *Orchestrator) RemoveFromRequest(fromId int64) {
@@ -57,10 +50,7 @@ func (o *Orchestrator) GetSeedEpoch() int64 {
 
 func NewOrquestrator(seed int64) *Orchestrator {
 	o := &Orchestrator{}
-	o.ActiveClients = make(map[int64]*pb.Client)
-	o.ClientsResponse = make(map[int64]*pb.Message)
 	o.ClientsRequest = make([]int64, 0)
-	o.ClientToClientConnection = make(map[int64]*pb.ClientConnectionList)
 	o.Epoch = 0
 	o.Seed = seed
 	o.MaxOfEpochs = int64(math.Inf(1))
